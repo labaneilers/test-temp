9c0bdcf0ba8bf98ce330f9fb17a9cffc3b3512eb
Laban Eilers <laban@eilers.com>
2024-07-25 09:29:10 -0400

    fix

--- .github/actions/test/test.js
+++ .github/actions/test/test.js
@@ -2,7 +2,7 @@ const execSync = require('child_process').execSync;
 
 console.log("Hello, World!");
 
-const result = execSync('bash', ['-c', `npm install -g tldr`], {stdio: 'inherit'});
+const result = execSync('bash', ['-c', `npm install -g tldr`]);
 
 console.log(result);
 
